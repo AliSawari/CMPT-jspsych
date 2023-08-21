c1370aadcdd9d4e4c3168541eed68f4caa762264
Ali Sawari <ali.dragon.swr@gmail.com>
2023-08-21 20:23:54 +0300

    falling back on the same port

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-const port = 80;
+const port = process.env.PORT || 3000
 
 app.listen(port, () => console.log('server is up on port', port));
